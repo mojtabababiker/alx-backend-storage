0acb106ce163fe88da4caf5352315c66442ed97c
mojtabababiker <mojmohammad98@gmail.com>
2024-05-10 04:26:08 +0200

    Edit task 9 , fix typo, project 0x00

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- Creating an index on names table with first letter of name column and the score
-CREATE INDEX idx_name_first ON names (name(1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
